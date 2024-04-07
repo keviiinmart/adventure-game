b6e3a3755b148742156269064edf6fb5f666a71e
Kevin Martinez <kevinmartinez55@gmail.com>
2024-04-06 23:47:32 -0500

    adds print statement to main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 #adventure game
 #import of the story
+import soj
 import storyfunc
 
 def story():
 	tav = storyfunc.getNameOfUser()
 	print(tav)
-	storyfunc.getStory(tav)	
-	
+	storyO = storyfunc.getStory(tav)	
+	if storyO == "Option 1":
+		story = soj.sojbegin
+		print(story)		
+
 story()
 
